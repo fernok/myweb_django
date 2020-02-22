756a4b961369230f24681a02dc9f3504b59d1521
Hyuhng Min Kim <minkim918@gmail.com>
2020-02-22 23:09:00 +0900

    0222 update

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Foreign Key: user to post(one to many relationship)
+    # on_delete: what happens to the post when the user is deleted?
+    #       delete the post? set the author to none?
+    #       CASCADE -> delete the post
+
+    def __str__(self):
+        return self.title
+    # what will be printed out
