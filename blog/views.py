acdb8cce95559dc85744ab41489f5749c36d5b9e
Hyuhng Min Kim <minkim918@gmail.com>
2020-02-23 00:33:53 +0900

    after database

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'HMK',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 # Handle traffic from homepage
 # Return what the user should see
 def home(request):
     context ={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
